274d6e7079c7bf1ff81cb0e7868c4e974bcfe832
Kamilcuk <kamilcukrowski@gmail.com>
2016-11-21 12:54:10 +0100

    Less artifacts from docker build - less to download

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,6 @@
 FROM finalduty/archlinux:latest
 MAINTAINER Kamil Cukrowski <kamilcukrowski@gmail.com>
-RUN pacman -Suy --downloadonly --noconfirm --noprogressbar arch-install-scripts sudo base-devel wget curl openssh sshfs rsync xmlto kmod git bc lzop coreutils linux-firmware mkinitcpio libpipeline yajl
-RUN echo "==> Download gcc-linaro to /opt" && \
-    dir=gcc-linaro-6.1.1-2016.08-x86_64_arm-linux-gnueabihf && \
-    curl -L -o /opt/$dir.tar.xz https://releases.linaro.org/components/toolchain/binaries/6.1-2016.08/arm-linux-gnueabihf/$dir.tar.xz
 COPY resources /
 RUN  /docker-entrypoint.sh
-RUN echo "==> Untar gcc-linaro (docker will tar it again)" && \
-    dir=gcc-linaro-6.1.1-2016.08-x86_64_arm-linux-gnueabihf && \
-    tar xfp /opt/$dir.tar.xz -C /opt && \
-    rm -f /opt/$dir.tar.xz && \
-    ln -s /opt/$dir /opt/gcc-linaro && \
-    echo "==> DONE!"
 
 
--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 set -xe
+echo "===> START $0 $*"
+
+linaro=gcc-linaro-6.1.1-2016.08-x86_64_arm-linux-gnueabihf
 
 ### not needed for finalduty/archlinux
 # ### init
+# echo "===> Make pacman work"
 # sed -e "/^SigLevel/s/SigLevel.*/SigLevel = Never/" -i /etc/pacman.conf
 # pacman -Sy --noconfirm pacman
 # pacman-db-upgrade
 # pacman -S --noconfirm pacman ca-certificates ca-certificates-mozilla archlinux-keyring
 
-## install all packages
-# libpipeline - binfmt-support
+echo "===> Update&install all needed packages"
+# libpipeline - binfmt-support dependency
 # yajl - package-query dependency
 pacman -Suy --noconfirm --noprogressbar arch-install-scripts sudo base-devel wget curl openssh sshfs rsync xmlto kmod git bc lzop coreutils linux-firmware mkinitcpio libpipeline yajl
 
-## enable sudo from nobody with nopasword, for 'sudo -u nobody makepkg -i' to work
+echo "===> enable sudo from nobody with nopasword, for 'sudo -u nobody makepkg -i' to work"
 echo "nobody ALL=(ALL:ALL) NOPASSWD: ALL" | (VISUAL="tee -a" EDITOR="tee -a" visudo)
 
-## FIX stupid bug when sudo inside docker
+echo "===> FIX stupid bug when sudo inside docker"
 #http://bit-traveler.blogspot.com/2015/11/sudo-error-within-docker-container-arch.html
 sed -e "/nice/s/\*/#*/" -i /etc/security/limits.conf
 
@@ -33,15 +37,26 @@ install_from_aur() {
 	rm -rf $tmpdir
 }
 
-## install yaourt
+echo "===> install yaourt"
 install_from_aur package-query
 install_from_aur yaourt
 
-## install binfmt and qemu-user-static
+echo "===> install binfmt and qemu-user-static"
 install_from_aur binfmt-support
 install_from_aur qemu-user-static
 
-## cleanup
+
+echo "==> Download $linaro to /opt"
+curl -L -o /opt/$linaro.tar.xz https://releases.linaro.org/components/toolchain/binaries/6.1-2016.08/arm-linux-gnueabihf/$linaro.tar.xz
+
+echo "==> Upacking linaro"
+tar xfp /opt/$linaro.tar.xz -C /opt
+rm -f /opt/$linaro.tar.xz
+ln -s /opt/$linaro /opt/gcc-linaro
+
+
+echo "===> cleanup"
 yes Y | pacman -Scc
 
+echo "===> DONE $0 $*"
 
